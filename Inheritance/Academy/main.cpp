849684ae5ace2af0a9fadc2f95def122ffc823e2
Student <Student@304-1>
2020-03-03 20:13:14 +0200

    +

--- Inheritance/Academy/main.cpp
+++ Inheritance/Academy/main.cpp
@@ -23,7 +23,7 @@ public:
 
 	void set_last_name(const std::string& last_name)
 	{
-		std::regex exp ("\\w");
+		std::regex exp("\\w");
 		this->last_name = last_name;
 
 	}
@@ -54,9 +54,9 @@ public:
 	{
 		std::cout.width(8);
 		/*std::ios_base::left;*/
-		std::cout /*<< "Фамилия: " */<< last_name << "\t";
+		std::cout /*<< "Фамилия: " */ << last_name << "\t";
 		std::cout.width(8);
-		std::cout /*<< "Имя: " */<< first_name << "\t";
+		std::cout /*<< "Имя: " */ << first_name << "\t";
 		std::cout.width(8);
 		std::cout /*<< "Возраст: "*/ << age << std::endl;
 	}
@@ -243,8 +243,11 @@ public:
 	}
 };
 //#define REGEX_NAME
-#define REGEX_GROOP
+//#define REGEX_GROOP
 //#define POLIMORPHISM
+//#define REGEX_EMAIL
+//#define REGEX_PHONE
+#define CONVERT_PHONE
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -290,7 +293,7 @@ void main()
 	std::string name;
 	std::cout << "Input name "; std::cin >> name;
 	std::regex rgx("[A-Z][a-z]{1,19}");
-	std::cout << std::regex_match(name, rgx, std::regex_constants::match_default)<<std::endl;
+	std::cout << std::regex_match(name, rgx, std::regex_constants::match_default) << std::endl;
 #endif // REGEX_NAME
 #ifdef REGEX_GROOP
 	std::string groop;
@@ -298,8 +301,34 @@ void main()
 	std::regex rgx("(ST|PS[ _\-](SB|VS))[ _\-](PU|PV|ITU|ITV|DU|DV)[_ \-]\\d{2}[abv]?");
 	getline(std::cin, groop);
 	std::regex rgx_replace(" |\-");
-	groop=std::regex_replace(groop, rgx_replace, "_", std::regex_constants::match_default);
+	groop = std::regex_replace(groop, rgx_replace, "_", std::regex_constants::match_default);
 	std::cout << std::regex_match(groop, rgx, std::regex_constants::match_default) << std::endl;
 	std::cout << groop << std::endl;
 #endif REGEX_GROOP
+#ifdef REGEX_EMAIL
+	std::string email = "vasya@gmail.jks.uaa";
+	/*std::cout << "Input E-mail: "; std::cin >> email;*/
+	std::regex rgx("\\w{3,50}@[a-z]{1,20}[.][a-z]{2,3}(.[a-z]{2,3})?");
+	std::cout << std::regex_match(email, rgx, std::regex_constants::match_default) << std::endl;
+#endif // REGEX_EMAIL
+#ifdef REGEX_PHONE
+	std::string phone = "+38(099)123-45-67";
+	//std::regex rgx("\\+\\d{2}\\([0]\\d{2}\\)\\d{3}(-\\d{2}){2}");
+	std::regex rgx(R"(\+\d{2}\([0]\d{2}\)\d{3}(-\d{2}){2})");
+	std::cout << std::regex_match(phone, rgx, std::regex_constants::match_default) << std::endl;
+#endif // REGEX_PHONE
+#ifdef CONVERT_PHONE
+	std::string phone = "380991234567";
+	std::regex rgx("\\d{12}");
+	if (std::regex_match(phone, rgx, std::regex_constants::match_default))
+	{
+		phone.insert(0, "+");
+		phone.insert(phone.size() - 10, "(");
+		phone.insert(phone.size() - 7, ")");
+		phone.insert(phone.size() - 4, "-");
+		phone.insert(phone.size() - 2, "-");
+	}
+		std::cout << phone << std::endl;
+#endif // CONVERT_PHONE
+
 }
