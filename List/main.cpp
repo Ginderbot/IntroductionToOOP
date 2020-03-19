3f9a00c79e78b97d022eda010f976aed24e62d69
Chalok <Chalok@CHALOK->
2020-03-19 21:30:58 +0200

    +

--- List/main.cpp
+++ List/main.cpp
@@ -25,22 +25,111 @@ public:
 	List()
 	{
 		head = tail = nullptr;
+		size = 0;
 		std::cout << "LConstructor:\t" << this << std::endl;
 	}
 	~List()
 	{
+		while (tail || head)
+		{
+			pop_front();
+			pop_back();
+		}
 		std::cout << "LDestructor:\t" << this << std::endl;
 	}
 	//adding elements
 	void push_front(int data)
 	{
-		if(head==nullptr)
+		if (head == nullptr)
 		{
 			head = tail = new Element(data);
+			size++;
 			return;
 		}
-		head =head->pPrev= new Element(data, head);
+		head = head->pPrev = new Element(data, head);
 		size++;
+	}
+	void push_back(int data)
+	{
+		if (head == nullptr)
+		{
+			head = tail = new Element(data);
+			size++;
+			return;
+		}
+		tail = tail->pNext = new Element(data,nullptr,tail);
+		size++;
+	}
+	void insert(int index, int data) 
+	{
+		Element* Temp;
+		if (index > size)throw std::exception("Error");
+		else if (index == 0)
+		{
+			push_front(data);
+				return;
+		}
+		else if (index == size)
+		{
+			push_back(data);
+			size++;
+			return;
+		}
+		else if (index <= size / 2)
+		{
+			Temp = head;
+			for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else if (index > size / 2)
+		{
+			Temp = tail;
+			for (int i = 0; i < size-1-index; i++)Temp = Temp->pPrev;
+		}
+		else
+		{
+			std::cout << "Error" << std::endl;
+			return;
+		}
+		/*Element* New = new Element(data);
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;*/
+		Temp =Temp->pNext=Temp->pNext->pPrev=new Element(data,head,Temp->pNext->pNext) ;
+		size++;
+	}
+	//delete elements
+	void pop_front()
+	{
+		if (head == tail)
+		{
+			delete head;
+			head = tail = nullptr;
+			if(size)size--;
+			return;
+		}
+		head = head->pNext;
+		delete head->pPrev;
+		head->pPrev = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (head == tail)
+		{
+			delete tail;
+			head = tail = nullptr;
+			if (size)size--;
+			return;
+		};
+		tail = tail->pPrev;
+		delete tail->pNext;
+		tail->pNext = nullptr;
+		size--;
+	}
+	void erase(int index)
+	{
+
 	}
 	//remuving elemtnts
 
@@ -49,13 +138,13 @@ public:
 	{
 		for (Element*Temp = head; Temp != nullptr; Temp = Temp->pNext)
 		{
-			std::cout << Temp->pPrev << "\t" <<Temp<<"\t"<< Temp->data<< "\t" << Temp->pNext<<std::endl;
+			std::cout << Temp->pPrev << "\t" << Temp << "\t" << Temp->data << "\t" << Temp->pNext << std::endl;
 		}
-		std::cout << "List size:\t" << size<<std::endl;
+		std::cout << "List size:\t" << size << std::endl;
 	}
 	void print_revers()const
 	{
-		for (Element*Temp=tail;Temp!=nullptr;Temp=Temp->pPrev)
+		for (Element*Temp = tail; Temp != nullptr; Temp = Temp->pPrev)
 		{
 			std::cout << Temp->pPrev << "\t" << Temp << "\t" << Temp->data << "\t" << Temp->pNext << std::endl;
 		}
@@ -98,8 +187,32 @@ void main()
 	List list;
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
+	list.print();
+	list.print_revers();
+	//std::cout << "____________________________add front___________________________________" << std::endl;
+	//list.push_front(100);
+	//list.print();
+	//list.print_revers();
+	//std::cout << "____________________________add back____________________________________" << std::endl;
+	//list.push_back(200);
+	//list.print();
+	//list.print_revers();
+	//std::cout << "__________________________delete front__________________________________" << std::endl;
+	//list.pop_front();
+	//list.print();
+	//list.print_revers();
+	//std::cout << "__________________________delete back___________________________________" << std::endl;
+	//list.pop_back();
+	//list.print();
+	//list.print_revers();
+	std::cout << "______________________________insert____________________________________" << std::endl;
+	int index;
+	int data;
+	std::cout << "¬ведите индекс: "; std::cin >> index;
+	std::cout << "¬ведите значение: "; std::cin >> data;
+	list.insert(index,data);
 	list.print();
 	list.print_revers();
 }
